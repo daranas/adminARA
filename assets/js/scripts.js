ccdde505e46530bde16784cc73b974b5db06ea46
Darana Sukma Vidya <darana.sv@gmail.com>
2021-05-03 01:26:25 +0700

    Remove unused code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,3 @@
 * Adminara v1.0.0 (http://darana.id/templates/adminara)
 * Copyright 2021 DaranaSV
 */
-(function($) {
-    "use strict";
-
-    $("#sidebarToggle").on("click", function(e) {
-        e.preventDefault();
-        $("body").toggleClass("is-toggled");
-    });
-})(jQuery);
