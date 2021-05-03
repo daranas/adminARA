89ae622be03f00adc0d7faefdedc6ddab7dd1a06
Darana Sukma Vidya <darana.sv@gmail.com>
2021-05-03 23:07:13 +0700

    init npm

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,15 @@
 * Adminara v1.0.0 (http://darana.id/templates/adminara)
 * Copyright 2021 DaranaSV
 */
+$(function () {
+    $('#sidebarNav').metisMenu();
+
+    let header = $('.ara-header');
+    let sidebar = $('.ara-sidebar');
+    let mainWrapper = $('.ara-main');
+    $('#sidebarToggle').on('click', function() {
+        header.toggleClass('collapsed');
+        sidebar.toggleClass('collapsed');
+        mainWrapper.toggleClass('collapsed');
+    })
+});
